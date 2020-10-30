3759e8f583d450f79c40f576957d9b3221c5eb76
MaiXiaochai <maixiaochai@qq.com>
2020-10-30 19:52:22 +0800

    +)upzip和unzips功能

--- file_utils.py
+++ file_utils.py
@@ -12,6 +12,7 @@
 from os import makedirs, listdir, remove
 from os.path import isdir, splitext, exists, join
 from shutil import copy, move
+from zipfile import ZipFile
 
 
 class FileUtils:
@@ -79,6 +80,56 @@ class FileUtils:
             dir_path = dir_path + slash
 
         return dir_path
+
+    @classmethod
+    def unzip(cls, zip_path: str, dest_dir: str = './'):
+        """解压zip压缩文件到 dest目录"""
+        with ZipFile(zip_path, 'r') as f:
+            files = f.namelist()
+            file_length = len(files)
+
+            for no, i in enumerate(files, 1):
+                f.extract(i, dest_dir)
+                cls.__progress_bar(no, file_length, zip_path)
+
+    @classmethod
+    def unzips(cls, src: str, dst: str):
+        """解压src目录下的zip文件，到dst目录"""
+        zip_files = cls.list_paths(src, suffix='zip')
+        if zip_files:
+            for z_file in zip_files:
+                cls.unzip(z_file, dst)
+
+    @staticmethod
+    def __progress_bar(portion, total, file_name):
+        from sys import stdout
+        from math import ceil
+        """
+        total 总数据大小，portion 已经传送的数据大小
+        :param portion: 已经接收的数据量
+        :param total:   总数据量
+        :return:        接收数据完成，返回True
+        """
+        # file_name 用于进度条显示文件名，
+        # 之前用zip_path全路径，太长
+        file_name = file_name.split('/')[-1] or file_name
+        part = total / 50  # 1%数据的大小
+        count = ceil(portion / part)
+        stdout.write('\r')
+        stdout.write(
+            ('[%-50s]%.2f%% | %d/%d | %s' % (
+                ('=' * count),
+                portion / total * 100,
+                portion,
+                total,
+                file_name)
+             )
+        )
+        stdout.flush()
+
+        if portion >= total:
+            stdout.write('\n')
+            return True
 
     @classmethod
     def list_paths(cls, dir_path, depth=0, suffix=None, key_str=None):
